d0bf7293439a1d83dadcb893e57e50e4425dc5f6
Graham Pinkston <graham.pinkston@gmail.com>
2024-10-07 10:22:53 -0500

    added IN to file 1 and started file 2

--- 01-Basic-Stored-Procedure-Structure.sql
+++ 01-Basic-Stored-Procedure-Structure.sql
@@ -5,29 +5,35 @@
             Simple SELECT queries.
             RETURN or OUT parameters.
 */
-
+/*
+PersonType = IN EM SP SC VC GC
+*/
 USE AdventureWorks2019;
 
 GO
 
 CREATE OR ALTER PROCEDURE GetPersonAndCity
-	@PersonType varchar(3)
+	@PersonType1 varchar(3),
+	@PersonType2 varchar(3) = '',
+	@PersonType3 varchar(3) = '',
+	@PersonType4 varchar(3) = ''
 AS
 BEGIN
 	SELECT
 		p.[FirstName],
 		p.[LastName],
-		a.[City]
+		a.[City],
+		p.[PersonType]
 	FROM [AdventureWorks2019].[Person].[Person] AS p
 	JOIN [AdventureWorks2019].[Person].[Address] AS a
 	ON a.[AddressID] = p.[BusinessEntityID]
-	WHERE p.[PersonType] = @PersonType;
+	WHERE p.[PersonType] IN ( @PersonType1, @PersonType2, @PersonType3 );
   
 	SELECT COUNT(*) AS PersonCount
 	FROM [AdventureWorks2019].[Person].[Person] AS p
-	WHERE p.[PersonType] = @PersonType;
+	WHERE p.[PersonType] IN ( @PersonType1, @PersonType2, @PersonType3 );
 END;
 
 GO
 
-EXEC [dbo].[GetPersonAndCity] @PersonType = 'EM';
+EXEC [dbo].[GetPersonAndCity] @PersonType1 = 'EM', @PersonType2 = 'SC', @PersonType3 = 'IN';
--- 02-Control-Flow-&-Logic.sql
+++ 02-Control-Flow-&-Logic.sql
@@ -4,3 +4,56 @@
             Use of IF/ELSE, WHILE, and LOOP.
             Conditional logic and branching.
 */
+
+
+USE AdventureWorks2019;
+GO
+
+CREATE OR ALTER PROCEDURE UpdateEmployeeSickLeaveHours
+AS
+BEGIN
+    DECLARE @EmployeeID INT, @SickLeaveHours INT;
+    DECLARE @TotalEmployees INT, @Counter INT = 1;
+
+    -- Get total number of employees in the HumanResources.Employee table
+    SELECT @TotalEmployees = COUNT(*) FROM HumanResources.Employee;
+    
+    -- WHILE loop to iterate through each employee
+    WHILE @Counter <= @TotalEmployees
+    BEGIN
+        -- Get the employee data one by one
+        SELECT TOP 1
+            @EmployeeID = BusinessEntityID,
+            @SickLeaveHours = SickLeaveHours
+        FROM HumanResources.Employee
+        WHERE BusinessEntityID NOT IN (SELECT TOP (@Counter - 1) BusinessEntityID FROM HumanResources.Employee ORDER BY BusinessEntityID)
+        ORDER BY BusinessEntityID;
+
+        -- Update SickLeaveHours based on conditions
+        IF @SickLeaveHours > 40
+        BEGIN
+            -- SickLeaveHours greater than 40, decrease by 5%
+            UPDATE HumanResources.Employee
+            SET SickLeaveHours = @SickLeaveHours * 0.95
+            WHERE BusinessEntityID = @EmployeeID;
+        END
+        ELSE IF @SickLeaveHours < 20
+        BEGIN
+            -- SickLeaveHours less than 20, increase by 10%
+            UPDATE HumanResources.Employee
+            SET SickLeaveHours = @SickLeaveHours * 1.10
+            WHERE BusinessEntityID = @EmployeeID;
+        END
+        ELSE
+        BEGIN
+            -- SickLeaveHours between 20 and 40, decrease by 2%
+            UPDATE HumanResources.Employee
+            SET SickLeaveHours = @SickLeaveHours * 0.98
+            WHERE BusinessEntityID = @EmployeeID;
+        END
+
+        -- Increment the counter
+        SET @Counter = @Counter + 1;
+    END
+END;
+GO
