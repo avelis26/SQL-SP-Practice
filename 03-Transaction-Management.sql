eb678da89ec18369902c6fb82266a75b1a6d2b11
Graham Pinkston <graham.pinkston@gmail.com>
2024-10-07 12:11:35 -0500

    updates

--- 03-Transaction-Management.sql
+++ 03-Transaction-Management.sql
@@ -5,3 +5,25 @@
             Handling concurrent operations with locks or isolation levels.
             Error recovery using transactions.
 */
+
+
+USE [AdventureWorks2019];
+GO
+
+CREATE OR ALTER PROCEDURE [GetEmployeeSickLeaveDetails]
+AS
+BEGIN
+	SELECT 
+		 p.[FirstName]
+		,p.[LastName]
+		,e.[SickLeaveHours]
+	FROM 
+		   [HumanResources].[Employee] AS e
+	JOIN 
+		   [Person].[Person] AS p ON e.[BusinessEntityID] = p.[BusinessEntityID]
+	ORDER BY 
+		 p.[LastName], p.[FirstName];
+END;
+GO
+
+EXEC	   [dbo].[GetEmployeeSickLeaveDetails]
